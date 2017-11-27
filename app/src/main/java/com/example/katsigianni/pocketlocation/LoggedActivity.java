f0459665309417aff45c2ade8fe565fcfdc7ba0f
dimitra95 <dimitra_gh@hotmail.com>
2017-11-28 01:53:22 +0200

    GPS development

--- app/src/main/java/com/example/katsigianni/pocketlocation/LoggedActivity.java
+++ app/src/main/java/com/example/katsigianni/pocketlocation/LoggedActivity.java
@@ -1,23 +1,107 @@
 package com.example.katsigianni.pocketlocation;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoggedActivity extends AppCompatActivity {
+
+     private Button b;
+    private TextView t;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged);
 
-     //   TextView welc=(TextView)findViewById(R.id.welcome);
-       // welc.setText(getIntent().getExtras().getString("fullname"));
+        //   TextView welc=(TextView)findViewById(R.id.welcome);
+        // welc.setText(getIntent().getExtras().getString("fullname"));
+
+        t = (TextView) findViewById(R.id.coors);
+        b = (Button) findViewById(R.id.buttoncoor);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                t.append("\n " + location.getLongitude() + " " + location.getLatitude());
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
 
 
+            }
 
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
+                        , 10);
+            }
+            return;
+        }else{
+            configureButton();
+
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                configureButton();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void configureButton() {
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //noinspection MissingPermission
+                locationManager.requestLocationUpdates("gps", 5000, 5, locationListener);
+
+            }
+        });
 
 
     }
+
+
+
 }
