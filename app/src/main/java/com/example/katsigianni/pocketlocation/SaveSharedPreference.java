db1230ed6f6937fdfc374048c4194c6937da25cd
KonstatinaKats <katsigiannikon@mail.com>
2018-03-05 18:39:24 +0200

    Add sharePreference // Test gps +tracking service

--- app/src/main/java/com/example/katsigianni/pocketlocation/SaveSharedPreference.java
+++ app/src/main/java/com/example/katsigianni/pocketlocation/SaveSharedPreference.java
@@ -12,10 +12,37 @@ public class SaveSharedPreference {
 
     static final String PREF_USER_NAME= "username";
     static final String PREF_USER_PERSONAL_NUMBER= "personal_number";
+    static final String LAST_LOCATION_LONGITUDE= "last_location_longitude";
+    static final String LAST_LOCATION_LATITUDE= "last_location_latitude";
 
     static SharedPreferences getSharedPreferences(Context ctx) {
         return PreferenceManager.getDefaultSharedPreferences(ctx);
     }
+    public static void setLastLocationLongitude (Context ctx, String lastLocationLongitude)
+    {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(LAST_LOCATION_LONGITUDE, lastLocationLongitude);
+        editor.commit();
+    }
+
+    public static String getLastLocationLongitude(Context ctx)
+    {
+        return getSharedPreferences(ctx).getString(LAST_LOCATION_LONGITUDE, "");
+    }
+
+    public static String getLastLocationLatitude(Context ctx)
+    {
+        return getSharedPreferences(ctx).getString(LAST_LOCATION_LATITUDE, "");
+    }
+
+    public static void setLastLocationLatitude (Context ctx, String lastLocationLatitude)
+    {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(LAST_LOCATION_LATITUDE, lastLocationLatitude);
+        editor.commit();
+    }
+
+
 
     public static void setUserName(Context ctx, String userName)
     {
