022baa9bea08f65eaa8391bab2b97663029747e0
KonstatinaKats <katsigiannikon@mail.com>
2018-04-05 00:48:54 +0300

    POST Coordinates and Activity (GPS flow) to database

--- app/src/main/java/com/example/katsigianni/pocketlocation/Services/ActivityRecognizedService.java
+++ app/src/main/java/com/example/katsigianni/pocketlocation/Services/ActivityRecognizedService.java
@@ -1,13 +1,18 @@
 package com.example.katsigianni.pocketlocation.Services;
 
 import android.app.IntentService;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.util.Log;
 
+import com.example.katsigianni.pocketlocation.Common;
+import com.example.katsigianni.pocketlocation.HTTPDataHandler;
 import com.example.katsigianni.pocketlocation.SaveSharedPreference;
 import com.google.android.gms.location.ActivityRecognitionResult;
 import com.google.android.gms.location.DetectedActivity;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -35,20 +40,27 @@ public class ActivityRecognizedService extends IntentService {
 
     private void handleDetectedActivities(List<DetectedActivity> probableActivities) {
 
-        Log.e("ActivityRecogition", "Longitude: " + SaveSharedPreference.getLastLocationLongitude(ActivityRecognizedService.this));
-        Log.e("ActivityRecogition", "Latitude: " + SaveSharedPreference.getLastLocationLatitude(ActivityRecognizedService.this));
+        String personal_number = SaveSharedPreference.getPersonalNumber(ActivityRecognizedService.this);
+        String latitude = SaveSharedPreference.getLastLocationLongitude(ActivityRecognizedService.this);
+        String longitude = SaveSharedPreference.getLastLocationLatitude(ActivityRecognizedService.this);
+
+        Log.e("ActivityRecogition", "Longitude: " + latitude);
+        Log.e("ActivityRecogition", "Latitude: " + longitude);
 
         for (DetectedActivity activity : probableActivities) {
+            String activityMessage = null;
             switch (activity.getType()) {
                 case DetectedActivity.IN_VEHICLE: {
                     if (activity.getConfidence() >= 65) {
                         Log.e("ActivityRecognition", "In Vehicle: " + activity.getConfidence());
+                        activityMessage = "In Vehicle";
                     }
                     break;
                 }
                 case DetectedActivity.ON_BICYCLE: {
                     if (activity.getConfidence() >= 65) {
                         Log.e("ActivityRecognition", "On Bicycle: " + activity.getConfidence());
+                        activityMessage = "On Bicycle";
                     }
                     break;
                 }
@@ -61,39 +73,82 @@ public class ActivityRecognizedService extends IntentService {
                 case DetectedActivity.RUNNING: {
                     if (activity.getConfidence() >= 65) {
                         Log.e("ActivityRecognition", "Running: " + activity.getConfidence());
+                        activityMessage = "Running";
                     }
                     break;
                 }
                 case DetectedActivity.STILL: {
                     if (activity.getConfidence() >= 65) {
                         Log.e("ActivityRecognition", "Still: " + activity.getConfidence());
+                        activityMessage = "Still";
                     }
                     break;
                 }
                 case DetectedActivity.TILTING: {
                     if (activity.getConfidence() >= 65) {
                         Log.e("ActivityRecognition", "Tilting: " + activity.getConfidence());
+                        activityMessage = "Tilting";
                     }
                     break;
                 }
                 case DetectedActivity.WALKING: {
                     if (activity.getConfidence() >= 65) {
                         Log.e("ActivityRecognition", "Walking: " + activity.getConfidence());
-//                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-//                        builder.setContentText("Are you walking?");
-//                        builder.setSmallIcon(R.mipmap.ic_launcher);
-//                        builder.setContentTitle(getString(R.string.app_name));
-//                        NotificationManagerCompat.from(this).notify(0, builder.build());
+                        activityMessage = "Walking";
                     }
                     break;
                 }
                 case DetectedActivity.UNKNOWN: {
                     if (activity.getConfidence() >= 65) {
                         Log.e("ActivityRecognition", "Unknown: " + activity.getConfidence());
+                        activityMessage = "Unknown";
                     }
                     break;
                 }
             }
+            new PostData(activityMessage, longitude, latitude ,new Date()).execute(Common.postNewLocation(personal_number));
+        }
+    }
+
+
+    private class PostData extends AsyncTask<String,String,String> {
+        ProgressDialog pd = new ProgressDialog(ActivityRecognizedService.this);
+        String activity;
+        String longitude;
+        String latitude;
+        Date date;
+
+        public PostData(String activity, String longitude, String latitude, Date date) {
+            this.activity = activity;
+            this.longitude = longitude;
+            this.latitude = latitude;
+            this.date = date;
+        }
+
+        @Override
+        protected  void onPreExecute(){
+            super.onPreExecute();
+            pd.setTitle("Please wait...");
+            pd.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params){
+            String urlString = params[0];
+
+            HTTPDataHandler hh = new HTTPDataHandler();
+            String json="{\"longitude\":\""+longitude+"\"," +
+                    "\"latitude\":\""+latitude+"\"," +
+                    "\"activity\":\""+activity+"\"," +
+                    "\"date\":\"" + date + "\" }";
+            hh.PostHTTPData(urlString,json);
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String s){
+            super.onPostExecute(s);
+            pd.dismiss();
         }
     }
 }
